e7f4c93b4bdb6449099fb76bf3bd1235e40c6831
silan2408 <silan_kahraman@hotmail.com>
2025-11-12 17:23:10 +0300

    12 kasım

---
+++ FirstReviewForTables.sql
@@ -0,0 +1,37 @@
+--Tabloların büyüklüğü nedir?
+
+--1
+select count(*) from `OLIST.olist_customers_dataset`;
+--olist_customers_dataset :99441 satır
+
+--2
+select count(*) from `OLIST.olist_geolocation_dataset`;
+--olist_geolocation_dataset :? satır
+
+--3
+select count(*) from `OLIST.olist_order_items_dataset`;
+--olist_order_items_dataset :112650 satır
+
+--4
+select count(*) from `OLIST.olist_order_payments_dataset`;
+--olist_order_payments_dataset :103886 satır
+
+--5
+select count(*) from `OLIST.olist_order_reviews_dataset`;
+--olist_order_reviews_dataset :99224 satır
+
+--6
+select count(*) from `OLIST.olist_orders_dataset`;
+--olist_orders_dataset :99441 satır
+
+--7
+select count(*) from `OLIST.olist_products_dataset`;
+--olist_products_dataset :32951 satır
+
+--8
+select count(*) from `OLIST.olist_sellers_dataset`;
+--olist_sellers_dataset :3095 satır
+
+--9
+select count(*) from `OLIST.product_category_name_translation`;
+--product_category_name_translation :71 satır
